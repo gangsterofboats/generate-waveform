dd086275dc52bf0dbfbe63ed4d0a8365e3730b07
Michael Wiseman <thestarslookdown@gmail.com>
2018-03-11 21:15:16 -0700

    Add Getopt::Long argument flags

--- genwavfm.pl
+++ genwavfm.pl
@@ -3,20 +3,30 @@ use strict;
 use warnings;
 use File::Basename;
 use File::Find;
+use Getopt::Long;
 use MP3::Info;
 use POSIX;
 use feature qw{ say };
 
-find(\&generate_waveform, '.');
+# Parse arguments
+GetOptions(
+    'input|i=s' => \my $input,
+    'output|o=s' => \my $output
+);
+my %options = ( input => $input, output => $output );
 
+# Ensure directory paths end in slashes
+$options{input} .= '/' unless substr($options{input}, -1) =~ m/\//;
+$options{output} .= '/' unless substr($options{output}, -1) =~ m/\//;
+
+# Sub to feed to File::Find sub
 sub generate_waveform
 {
     my $file = $File::Find::name;
     if ($file =~ /\.mp3$/i)
     {
-        my $artist = dirname(dirname($file));
+        my $artist = basename(dirname(dirname($file)));
         my $album = basename(dirname($file));
-        $artist =~ s/\.\///g;
         my $info = get_mp3info(basename($file));
         my $length = $info->{SECS};
         $length = ceil($length);
@@ -35,8 +45,10 @@ sub generate_waveform
             $output_filename =~ s/mp3/png/g;
         }
 
-        $output_filename = '/home/michael/Temp/' . $output_filename;
-        $file =~ s/\./\/home\/michael\/Music/;
+        $output_filename = $options{output} . $output_filename;
         system('audiowaveform', '-i', $file, '-o', $output_filename, '-e', $length, '-w', '1600', '-h', '500');
     }
 }
+
+# Main part of script
+find(\&generate_waveform, $options{input});
