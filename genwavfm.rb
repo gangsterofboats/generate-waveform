d37a623e518ba7f5fa6e581fc014f0560223d76d
Michael Wiseman <thestarslookdown@gmail.com>
2018-03-11 21:17:59 -0700

    Slight cleanup

--- genwavfm.rb
+++ genwavfm.rb
@@ -2,26 +2,31 @@
 require 'mp3info'
 require 'optparse'
 
-# Input/output directory options
+# Parse arguments
 options = {}
 optparse = OptionParser.new do |opts|
-  opts.banner = "Usage: genwavfm.rb --input PATH --output PATH"
+  opts.banner = 'Usage: genwavfm.rb --input PATH --output PATH'
 
-  opts.on("-i", "--input PATH", "Input path") do |i|
+  opts.on('-i', '--input PATH', 'Input path') do |i|
     options[:input] = i
   end
 
-  opts.on("-o", "--output PATH", "Output path") do |o|
+  opts.on('-o', '--output PATH', 'Output path') do |o|
     options[:output] = o
   end
 end
 optparse.parse!
+
+# Ensure directory paths end in slashes
+options[:input] += '/' unless options[:input][-1] == '/'
+options[:output] += '/' unless options[:output][-1] == '/'
 
 mp3s = Dir.glob(options[:input] + '**/*.mp3')
 mp3s.each do |f|
   # Get the artist and album from the filepath
   artist = File.basename(File.dirname(File.dirname(f)))
   album = File.basename(File.dirname(f))
+
   # Workaround for audiowaveform bug; seemingly it should read length
   # correctly and automatically, but it doesn't for me
   length = Mp3Info.open(f).length
