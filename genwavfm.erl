f6382e37746088575d7338cd3c88ade9493778c6
gangsterofboats <36470882+gangsterofboats@users.noreply.github.com>
2018-03-13 17:52:06 -0700

    Update genwavfm.erl

--- genwavfm.erl
+++ genwavfm.erl
@@ -27,20 +27,3 @@ generate_waveform(List) ->
             Oupufina = re:replace(Opfn, " ", "_", [global, {return, list}]),
             io:format("~s\n", [Oupufina])
     end.
-    
-
-
-%% io:format("~s\n", [string:join(Args, " ")]),
-
-    %% if artist == 'Various Artists':
-        %% output_filename = 'VA_' + album + '_' + os.path.basename(f)
-        %% output_filename = output_filename.replace(' ', '_')
-        %% output_filename = output_filename.replace('mp3', 'png')
-    %% else:
-        %% output_filename = artist + '_' + album + '_' + os.path.basename(f)
-        %% output_filename = output_filename.replace(' ', '_')
-        %% output_filename = output_filename.replace('mp3', 'png')
-
-    %% output_filename = '/home/michael/Temp/' + output_filename
-
-    %% subprocess.run(['audiowaveform', '-i', os.path.abspath(f), '-o', output_filename, '-e', str(length), '-w', '1600', '-h', '500'])
